3e2e1917636d92cc4cef6a7a5395e1dda6976fae
tugasbrandself8 <nataliasonsitiskiodgqqg@gmail.com>
2025-06-09 10:15:49 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,9 @@
+
+// main.cpp - Example file to set language to C++
+
+#include <iostream>
+
+int main() {
+    std::cout << "Marvel Rivals Hack Loaded Successfully!" << std::endl;
+    return 0;
+}
